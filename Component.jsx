ef6eeb814488213ecf17dec6d4df0d007206c4f9
jefta-jose <ndegwajeff4@gmail.com>
2024-12-30 11:45:15 +0300

    importing images using one file

---
+++ Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import images from './images'
+
+const Component = () => {
+  return (
+    <div>
+        <img src={images.image1} alt="" />
+    </div>
+  )
+}
+
+export default Component
---
+++ images.js
@@ -0,0 +1,10 @@
+import image1 from '../assets/email.png';
+import image2 from '../assets/mobile.png';
+import image3 from '../assets/api.png';
+
+
+export default {
+    image1,
+    image2,
+    image3
+};
